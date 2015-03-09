85703160c80f98bf1b2a73bdb80654e24cc833b6
Nicholas Zhang <kaihong.zhang@funplusgame.com>
2015-03-09 18:05:29 +0800

    str rand

---
+++ str/rand.go
@@ -0,0 +1,15 @@
+package str
+
+import (
+	"crypto/rand"
+)
+
+func Rand(str_size int) string {
+	alphanum := "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	var bytes = make([]byte, str_size)
+	rand.Read(bytes)
+	for i, b := range bytes {
+		bytes[i] = alphanum[b%byte(len(alphanum))]
+	}
+	return string(bytes)
+}
