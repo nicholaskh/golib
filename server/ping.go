d51929155484eb47e1c9a0f4d3326381fd97d28f
Nicholas Zhang <kaihong.zhang@funplusgame.com>
2015-05-31 16:42:26 +0800

    ping server

---
+++ server/ping.go
@@ -0,0 +1,41 @@
+package server
+
+import (
+	"net"
+
+	log "github.com/nicholaskh/log4go"
+)
+
+const (
+	PACKET_PING = "PING"
+	PACKET_PONG = "PONG"
+)
+
+func StartPingServer(port int) {
+	listenConn, err := net.ListenUDP("udp4", &net.UDPAddr{
+		IP:   net.IPv4(0, 0, 0, 0),
+		Port: port,
+	})
+	if err != nil {
+		panic(err)
+	}
+	for {
+		data := make([]byte, 1000)
+		n, remoteAddr, err := listenConn.ReadFromUDP(data)
+		if err != nil {
+			log.Error("Read from %s error", remoteAddr)
+			continue
+		}
+		rev := string(data[:n])
+		if rev == PACKET_PING {
+			log.Info("Ping from %s", remoteAddr)
+			_, err := listenConn.WriteTo([]byte(PACKET_PONG), remoteAddr)
+			if err != nil {
+				log.Error("Write to %s error: %s", remoteAddr, err.Error())
+			}
+		} else {
+			log.Warn("Received unknown packet: %s", rev)
+			continue
+		}
+	}
+}
