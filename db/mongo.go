3eb8855261563c50a10bfd714a5e4696cc03763a
Nicholas Zhang <kaihong.zhang@funplusgame.com>
2015-06-30 10:49:16 +0800

    mongo pool

--- db/mongo.go
+++ db/mongo.go
@@ -1,22 +1,47 @@
 package db
 
 import (
-    "fmt"
-    "labix.org/v2/mgo"
+	"fmt"
+	"labix.org/v2/mgo"
 )
 
 var (
-    mgoSession *mgo.Session
+	mgoSession *mgo.Session
 )
 
 func MgoSession(addr string) *mgo.Session {
-    if mgoSession == nil {
-        var err error
-        mgoSession, err = mgo.Dial(addr)
-        if err != nil {
-            panic(fmt.Sprintf("Connect to mongo error: %s", err.Error()))
-        }
-    }
-    return mgoSession
+	if mgoSession == nil {
+		var err error
+		mgoSession, err = mgo.Dial(addr)
+		if err != nil {
+			panic(fmt.Sprintf("Connect to mongo error: %s", err.Error()))
+		}
+	}
+	return mgoSession
 }
 
+type MgoSessionPool struct {
+	pool chan *mgo.Session
+}
+
+func NewMgoSessionPool(addr string, size int) *MgoSessionPool {
+	this := new(MgoSessionPool)
+	this.pool = make(chan *mgo.Session, size)
+	for i := 0; i < size; i++ {
+		mgoSession, err := mgo.Dial(addr)
+		if err != nil {
+			panic(fmt.Sprintf("Connect to mongo error: %s", err.Error()))
+		}
+		this.pool <- mgoSession
+	}
+
+	return this
+}
+
+func (this *MgoSessionPool) Get() *mgo.Session {
+	return <-this.pool
+}
+
+func (this *MgoSessionPool) Put(connection *mgo.Session) {
+	this.pool <- connection
+}
