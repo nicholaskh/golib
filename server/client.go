ef629da40c0967800bc96bead1e4b08a38a10481
UniSky <1832789858@qq.com>
2017-06-28 11:25:22 +0800

    add method WriteFormatMsg

--- server/client.go
+++ server/client.go
@@ -3,6 +3,9 @@ package server
 import (
 	"net"
 	"sync"
+	"errors"
+	"bytes"
+	"encoding/binary"
 
 	log "github.com/nicholaskh/log4go"
 )
@@ -27,7 +30,7 @@ func (this *Client) WriteMsg(msg string) error {
 	data := this.Proto.Marshal([]byte(msg))
 	this.Mutex.Lock()
 	if !this.IsConnected() {
-		return nil
+		return errors.New("link has been closed")
 	}
 	_, err := this.Conn.Write(data)
 	this.Mutex.Unlock()
@@ -38,18 +41,55 @@ func (this *Client) WriteMsg(msg string) error {
 	return err
 }
 
-func (this *Client) WriteBinMsg(msg []byte) {
+func (this *Client) WriteFormatMsg(op, body string) error {
+
+	dataBuff := bytes.NewBuffer([]byte{})
+
+	opBytes := []byte(op)
+	var bodyBytes []byte
+	if body != ""{
+		bodyBytes = []byte(body)
+	}
+
+	// write op to dataBuff
+	buf := bytes.NewBuffer([]byte{})
+	binary.Write(buf, binary.BigEndian, int32(len(opBytes)))
+	dataBuff.Write(buf.Bytes())
+
+	buf.Reset()
+	binary.Write(buf, binary.BigEndian, opBytes)
+	dataBuff.Write(buf.Bytes())
+
+	// write body to dataBuff
+	if body != "" {
+		buf.Reset()
+		binary.Write(buf, binary.BigEndian, int32(len(bodyBytes)))
+		dataBuff.Write(buf.Bytes())
+
+		buf.Reset()
+		binary.Write(buf, binary.BigEndian, bodyBytes)
+		dataBuff.Write(buf.Bytes())
+	}
+
+	// write into io
+	return this.WriteBinMsg(dataBuff.Bytes())
+
+}
+
+func (this *Client) WriteBinMsg(msg []byte) error {
 	data := this.Proto.Marshal(msg)
 	this.Mutex.Lock()
 	if !this.IsConnected() {
-		return
+		return errors.New("link has been closed")
 	}
-	this.Conn.Write(data)
+	_, err := this.Conn.Write(data)
 	this.Mutex.Unlock()
 
 	if this.conn_type == CONN_TYPE_LONG_POLLING {
 		this.Close()
 	}
+
+	return err
 }
 
 func (this *Client) IsConnected() bool {
